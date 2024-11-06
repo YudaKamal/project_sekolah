2bb6959165ecf129b173d7e9a6562efc05ccd904
YudaKamal <yudaakamall@gmail.com>
2024-11-06 11:34:10 +0700

    1

--- admin/prosestambah.php
+++ admin/prosestambah.php
@@ -4,7 +4,7 @@
     $harga = $_POST['harga'];
     $deskripsi = $_POST['deskripsi'];
     $kategori = $_POST['kategori'];
-    $nama_foto = $_FILES['foto']['nama'];
+    $nama_foto = $_FILES['foto']['name'];
     $lokasi_tmp = $_FILES['foto']['tmp_name'];
     $nama_baru = time().$nama_foto;
 
@@ -12,10 +12,10 @@
     $sql = "INSERT INTO produk (`id`, `nama`, `harga`, `deskripsi`, `kategori`, `foto`) values('', '$nama', '$harga', '$deskripsi', '$kategori','$nama_baru')";
     $query = mysqli_query($koneksi, $sql);
     
-    move_uploaded_file($lokasi_tmp,"../asset/$nama_baru");
     
     header("location: halaman_admin.php");
     
+    move_uploaded_file($lokasi_tmp,"../asset/$nama_foto");
 
 
 ?>  
